40e660d3e7e9bf675c890591a4ca0972289f2e5e
Seungwoo Kim <seungwookim08@gmail.com>
2018-10-19 01:08:04 -0400

    csv file created with weird encoding

--- Kijiji/parsing.py
+++ Kijiji/parsing.py
@@ -1,20 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import parser
+import re
+
 
 response = requests.get('https://www.kijiji.ca/b-achat-et-vente/ville-de-montreal/c10l1700281')
 
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'lxml')
 #  soup = BeautifulSoup(response.text, "lxml").text
 
 rentPrice = soup.find_all('div', 'price')
 
-print(rentPrice)
-div_tag = rentPrice.div.extract()
+print (rentPrice[0])
 
-print(div_tag)
+with open('price.csv', 'w', encoding='UTF-8') as csvfile:
+   priceWriter = csv.writer(csvfile)
+   for row in rentPrice:
+       priceWriter.writerow(row)
 
-with open('price.csv', 'wb') as csvfile:
-   priceWriter = csv.writer(csvfile, delimiter=' ')
-
-   priceWriter.writerows(rentPrice)
+# div_tag = rentPrice.div.extract()
+#
+# print(div_tag)
+#
+# with open('price.csv', 'wb') as csvfile:
+#    priceWriter = csv.writer(csvfile, delimiter=' ')
+#
+#    priceWriter.writerows(rentPrice)
