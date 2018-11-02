adb76dd2577fc2fe7bbea8bd99bb60acd74d37e5
Elijah Micho <elijah.micho@hotmail.com>
2018-11-01 20:30:15 -0400

    update

--- Kijiji/Elijah/rentScrape.py
+++ Kijiji/Elijah/rentScrape.py
@@ -10,34 +10,36 @@ url = pyperclip.paste()  # copy kijiji url from clipboard
 outputFile = open('price.csv', 'w', newline='') #create csv file in write mode
 outputWriter = csv.writer(outputFile, delimiter=",")
 
-z = 0
-while z <= 235:
-    res = requests.get(url) #downloads url and checks if there are no errors
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text, "html.parser")   #turns to html content
+res = requests.get(url) #downloads url and checks if there are no errors
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, "html.parser")   #turns to html content
 
-    informationDIV = soup.select('div .info-container') #information we want is in a div called .info-container
+informationDIV = soup.select('div .info-container') #information we want is in a div called .info-container
 
-    titleList = []      #empty dictionary to store title and rent
-    rentList = []
-    
-    if i not 26:
-        for i in range(len(informationDIV)):#loops over how # of results per page and puts data intodictionary
-            titles = soup.select('div .info-container > .title > a')
-            removeNewline = titles[i].getText()
-            titleList.append(removeNewline.replace("\n", ""))
-            outputWriter.writerow([titleList[i]])
+titleList = []      #empty dictionary to store title and rent
+rentList = []
+
+
+for j in range(4):
+    for i in range(len(informationDIV)):#loops over how # of results per page and puts data intodictionary
+        titles = soup.select('div .info-container > .title > a')
+        removeNewline = titles[i].getText()
+        titleList.append(removeNewline.replace("\n", ""))
+        outputWriter.writerow([titleList[i]])
                     
-            rents = soup.select('div .price')
-            removeNewline = rents[i].getText()
-            rentList.append(removeNewline.replace("\n", "").replace("\xa0",""))
-            outputWriter.writerow([rentList[i]])
-    else:
-        nextpageLink = soup.select('a[title="Suivante"]')[0]
-        url = 'http://www.kijiji.ca' + nextpageLink.get('href')
-        res = requests.get(url)
-        res.raise_for_status()
-        soup = bs4.BeautifulSoup(res.text, "html.parser")
+        rents = soup.select('div .price')
+        removeNewline = rents[i].getText()
+        rentList.append(removeNewline.replace("\n", "").replace("\xa0",""))
+        outputWriter.writerow([rentList[i]])
+    nextpageLink = soup.select('a[title="Suivante"]')[0]
+    url = 'http://www.kijiji.ca' + nextpageLink.get('href')
+    print(url)
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    print('hello')
+
+
 
 
 outputFile.close()
