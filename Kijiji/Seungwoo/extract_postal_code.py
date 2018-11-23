b1d572d13f028b06dbf51ff50ee6b3e4cb92570e
Seungwoo Kim <seungwookim08@gmail.com>
2018-11-22 21:57:08 -0500

    fix bugs

--- Kijiji/Seungwoo/extract_postal_code.py
+++ Kijiji/Seungwoo/extract_postal_code.py
@@ -1,5 +1,9 @@
 import pandas
 
 df = pandas.read_csv('kijiji_full_result.csv',delimiter=',')
-df['Postal Code'] = df['Address'].str.extract(r'([A-Z][0-9][A-Z]\s[0-9][A-Z][0-9])')
-df.to_csv('kijiji_full_result.csv')
+df['Postal Code'] = df['Address'].str.extract(r'([a-zA-Z][0-9][a-zA-Z]\s*[0-9][a-zA-Z][0-9])')
+df.to_csv('kijiji_full_result_with_postal_code.csv', index=False)
+
+df = pandas.read_csv('kijiji_full_result_with_postal_code.csv',delimiter=',')
+df = df.loc[df['Postal Code'].isnull()]
+df.to_csv('kijiji_full_result_non_postal_code.csv', index=False)
