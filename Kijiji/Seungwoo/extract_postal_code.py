de5ac223cd95b3c159c9e93abc8dae33fc6ca026
Seungwoo Kim <seungwookim08@gmail.com>
2018-11-22 20:46:01 -0500

    Extract postal code

--- Kijiji/Seungwoo/extract_postal_code.py
+++ Kijiji/Seungwoo/extract_postal_code.py
@@ -2,4 +2,4 @@ import pandas
 
 df = pandas.read_csv('kijiji_full_result.csv',delimiter=',')
 df['Postal Code'] = df['Address'].str.extract(r'([A-Z][0-9][A-Z]\s[0-9][A-Z][0-9])')
-
+df.to_csv('kijiji_full_result.csv')
